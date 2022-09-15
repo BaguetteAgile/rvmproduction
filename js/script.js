16b00fe345d8cc65cac085d48315d9ff33d06f65
BaguetteAgile <35597855+BaguetteAgile@users.noreply.github.com>
2022-09-15 22:20:43 +0200

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1120},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-marketing","displayName":"Marketing","link":{"linkType":"LinkTypePage","href":"#!page-marketing"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-realisations","displayName":"realisations","link":{"linkType":"LinkTypePage","href":"#!page-realisations"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-contact","displayName":"contact","link":{"linkType":"LinkTypePage","href":"#!page-contact"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-web","displayName":"web","link":{"linkType":"LinkTypePage","href":"#!page-web"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-politique-de-cookies","displayName":"politique-de-cookies","link":{"linkType":"LinkTypePage","href":"#!page-politique-de-cookies"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-mentions-l-gales","displayName":"Mentions-Légales","link":{"linkType":"LinkTypePage","href":"#!page-mentions-l-gales"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
+    window.silex.data = {"site":{"width":1251},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-marketing","displayName":"Marketing","link":{"linkType":"LinkTypePage","href":"#!page-marketing"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-realisations","displayName":"realisations","link":{"linkType":"LinkTypePage","href":"#!page-realisations"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-contact","displayName":"contact","link":{"linkType":"LinkTypePage","href":"#!page-contact"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-web","displayName":"web","link":{"linkType":"LinkTypePage","href":"#!page-web"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+<script src="https://www.google.com/recaptcha/api.js?render=6LcUJQEiAAAAAAHP2PviEOv9ZI6nu_C5FOLQ0muF">
+
+      function onClick(e) {
+        e.preventDefault();
+        grecaptcha.ready(function() {
+          grecaptcha.execute('6LcUJQEiAAAAAAHP2PviEOv9ZI6nu_C5FOLQ0muF', {action: 'submit'}).then(function(token) {
+              // Add your logic to submit to your backend server here.
+          });
+        });
+      }
+  

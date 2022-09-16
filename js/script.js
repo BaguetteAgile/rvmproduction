2c627820dd61687b4373bf6ce8cbaaac43f43867
BaguetteAgile <35597855+BaguetteAgile@users.noreply.github.com>
2022-09-16 09:27:02 +0200

    Batch update

--- js/script.js
+++ js/script.js
@@ -2,7 +2,11 @@
 
     window.silex = window.silex || {}
     window.silex.data = {"site":{"width":1251},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-marketing","displayName":"Marketing","link":{"linkType":"LinkTypePage","href":"#!page-marketing"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-realisations","displayName":"realisations","link":{"linkType":"LinkTypePage","href":"#!page-realisations"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-contact","displayName":"contact","link":{"linkType":"LinkTypePage","href":"#!page-contact"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-web","displayName":"web","link":{"linkType":"LinkTypePage","href":"#!page-web"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
-<script src="https://www.google.com/recaptcha/api.js?render=6LcUJQEiAAAAAAHP2PviEOv9ZI6nu_C5FOLQ0muF">
+<script type="text/javascript">
+  var onloadCallback = function() {
+    alert("grecaptcha is ready!");
+  };
+
 
       function onClick(e) {
         e.preventDefault();
